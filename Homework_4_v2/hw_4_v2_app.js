f9f30a06c841f797d0da360ebbe26ed630af911d
lpiment1 <44218362+lpiment1@users.noreply.github.com>
2019-11-25 20:08:50 -0800

    Delete hw_4_v2_app.js

--- Homework_4_v2/hw_4_v2_app.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker) {
-    window.addEventListener('load', () => {
-	   navigator.serviceWorker.register('./hw_4_v2_service-worker.js')
-	      .then(registration => console.log('SW registered'))
-		  .catch(err => console.log(`SW not registered - Error: ${err}`))
-    })
-} else {
-    console.log('Service Worker is not supported in this browser.')
-}
